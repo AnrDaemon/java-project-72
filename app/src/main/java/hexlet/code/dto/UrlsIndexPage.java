d0a781fb069af9f70322f7668d4aae1cb2401af7
Andrey Repin <anrdaemon@yandex.ru>
2026-04-13 00:29:20 +0300

    Added page DTO's

---
+++ app/src/main/java/hexlet/code/dto/UrlsIndexPage.java
@@ -0,0 +1,17 @@
+package hexlet.code.dto;
+
+import hexlet.code.model.Url;
+
+import java.util.List;
+import java.util.Map;
+
+import hexlet.code.model.UrlCheck;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class UrlsIndexPage extends BasePage {
+    private List<Url> urls;
+    private Map<Integer, UrlCheck> checks;
+}
---
+++ app/src/main/java/hexlet/code/dto/UrlsShowPage.java
@@ -0,0 +1,15 @@
+package hexlet.code.dto;
+
+import hexlet.code.model.Url;
+import hexlet.code.model.UrlCheck;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class UrlsShowPage extends BasePage {
+    private Url url;
+    private List<UrlCheck> checks;
+}
