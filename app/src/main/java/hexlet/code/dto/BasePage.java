0675e662cedc25f38bb4a3affd2a6c92cb150981
Andrey Repin <anrdaemon@yandex.ru>
2026-04-16 02:56:47 +0300

    Fixed basepage

--- app/src/main/java/hexlet/code/dto/BasePage.java
+++ app/src/main/java/hexlet/code/dto/BasePage.java
@@ -1,11 +1,16 @@
 package hexlet.code.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public abstract class BasePage {
+
     private String flash;
     private String alertType;
+
+    protected String name = "";
 }
